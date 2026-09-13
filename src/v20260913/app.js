27d9daacd910e9392733f20c6d45b7dfc4572acc
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-09-13 14:16:40 +0900

    Cache-bust deployed PNG card data

--- src/v20260913/app.js
+++ src/v20260913/app.js
@@ -1,5 +1,5 @@
 import { GameEngine, CARD_TYPES, PHASES } from './game-engine.js';
-import { CHARACTERS, createMadokaDeck, createMamiDeck } from './card-data.js';
+import { CHARACTERS, createMadokaDeck, createMamiDeck } from './card-data.js?v=png1';
 
 const $ = (sel) => document.querySelector(sel);
 let engine;
