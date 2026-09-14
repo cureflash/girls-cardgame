0ca5c8101e0766f93fc86c852fc16050ec8895b1
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-09-15 01:23:54 +0900

    Disallow opening shield from attacking side

--- src/character-engine.js
+++ src/character-engine.js
@@ -1,2 +1,20 @@
 // Canonical browser/analysis entry point for all current character rules.
-export { GameEngine, PHASES, CARD_TYPES } from './nagisa-engine.js';
+import { GameEngine as NagisaGameEngine, PHASES, CARD_TYPES } from './nagisa-engine.js';
+
+export { PHASES, CARD_TYPES };
+
+export class GameEngine extends NagisaGameEngine {
+  canActivateMagic(playerIndex, card) {
+    const battle = this.state.battle;
+    if (
+      card?.effect === 'nullifyDamage'
+      && battle
+      && !battle.direct
+      && playerIndex === battle.attackerPlayer
+      && (this.state.chain?.length ?? 0) === 0
+    ) {
+      return false;
+    }
+    return super.canActivateMagic(playerIndex, card);
+  }
+}
